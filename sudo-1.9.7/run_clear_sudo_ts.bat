cc7e939fa99c4723e3dcdf5cc07162d922e0ba1b
Marcelo <4familias@gmail.com>
2024-04-23 22:21:43 -0300

    Agregado vagrantfile y archivos necesarios

--- sudo-1.9.7/run_clear_sudo_ts.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-/c/msys64/usr/bin/rm.exe -f /d/Calilegua_MisProyectos/devops-2024-q2-a/practico-01/sudo-1.9.7/ts/*.bat
-
--- sudo-1.9.7/start_clear_sudo_ts.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "D:\Calilegua_MisProyectos\devops-2024-q2-a\practico-01\sudo-1.9.7\run_clear_sudo_ts.bat"
